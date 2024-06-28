828e8d5643131c297de12b2092dc2fc7c6908f70
Abdurrasyid Muhasibi <com.syid@gmail.com>
2024-06-28 07:55:29 +0700

    fix code

--- bank_account.js
+++ bank_account.js
@@ -1,35 +1,35 @@
 let balance = 0;
 
 function deposit() {
-    let money = window.prompt("Deposit: ");
-    if (validateInput(money)) {
-        balance += Number(money);
-        updateBalanceElement();
-        alert(`Balance: ${balance}`);
-    } else {
-        alert('Invalid Input!');
-    }
+  let money = window.prompt("Deposit: ");
+  if (validateInput(money)) {
+    balance += Number(money);
+    updateBalanceElement();
+    alert(`Balance: ${balance}`);
+  } else {
+    alert("Invalid Input!");
+  }
 }
 
 function withdraw() {
-    let money = window.prompt("Withdraw: ");
-    if (validateInput(money)) {
-        if (money > balance) {
-            alert('Balance is not enough!');
-        } else {
-            balance -= Number(money);
-            updateBalanceElement();
-            alert(`Balance: ${balance}`);
-        }
+  let money = window.prompt("Withdraw: ");
+  if (validateInput(money)) {
+    if (money > balance) {
+      alert("Balance is not enough!");
     } else {
-        alert('Invalid Input!');
+      balance -= Number(money);
+      updateBalanceElement();
+      alert(`Balance: ${balance}`);
     }
+  } else {
+    alert("Invalid Input!");
+  }
 }
 
 function updateBalanceElement() {
-    document.getElementById("balance").textContent = `Balance: ${balance}`;
+  document.getElementById("balance").textContent = `Balance: ${balance}`;
 }
 
 function validateInput(money) {
-    return !isNaN(money) && money >= 0;
+  return !isNaN(money) && money >= 0;
 }
